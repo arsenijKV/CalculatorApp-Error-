49e03e76f2f9f3d2fc8bd374d747c43b9afbce3d
arsenijKV <163118921+arsenijKV@users.noreply.github.com>
2025-07-07 17:33:59 +0300

    Add files via upload

---
+++ CalculatorApp.Test/CalculatorLogicTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using Methods;
+
+namespace CalculatorApp.Tests
+{
+    public class CalculatorLogicTests
+    {
+        private CalculatorState state;
+        private CalculatorLogic logic;
+
+        [SetUp]
+        public void Setup()
+        {
+            state = new CalculatorState();
+            logic = new CalculatorLogic(state);
+        }
+
+        [Test]
+        public void Addition_WorksCorrectly()
+        {
+            state.result = 5;
+            state.curentInput = "10";
+            state.operation = "+";
+            state.operationPending = true;
+
+            logic.Evaluate();
+
+            Assert.That(state.result, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void Division_ByZero_SetsError()
+        {
+            state.result = 10;
+            state.curentInput = "0";
+            state.operation = "/";
+            state.operationPending = true;
+
+            logic.Evaluate();
+
+            Assert.That(state.curentInput, Is.EqualTo("Error"));
+        }
+
+        [Test]
+        public void InvalidInput_SetsError()
+        {
+            state.result = 5;
+            state.curentInput = "abc";
+            state.operation = "*";
+            state.operationPending = true;
+
+            logic.Evaluate();
+
+            Assert.That(state.curentInput.StartsWith("Error"), Is.True);
+        }
+    }
+}
